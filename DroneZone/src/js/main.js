ecf2ec88c7399423bb51e1d30fdd91489cdd3ca4
Rostislav Ses <rostislavses97@gmail.com>
2019-03-13 23:26:40 +0200

    Added: smooth scroll and fixed scroll menu

--- DroneZone/src/js/main.js
+++ DroneZone/src/js/main.js
@@ -1,4 +1,7 @@
-// Mouse down effect
+/* Smooth scroll ====================================*/
+
+/* Smooth scroll version 1*/
+
 // $(document).ready(function(){
 // 	$('.scroll-down').click(function(){
 // 		$id = $(this);
@@ -8,20 +11,61 @@
 // });
 
 
-// ver.2
-// $(document).ready(function(){
-// 	$('.scroll-down__link').click(function(e){
-// 		e.preventDefault();
-// 		$id = $(this).attr('href');
-// 		$('body,html').animate({
-// 			scrollTop: $($id).offset().top -20}, 750);
-// 	});
+/* Smooth scroll version 2*/
+// $(function() {
+//   $('a[href="#about"], ').on('click', function(e) {
+//     e.preventDefault();
+
+//     $('html, body').animate({ 
+//     	scrollTop: $($(this).attr('href')).offset().top }, 800, 'linear');
+//   });
 // });
-$(function() {
-  $('a[href="#about"]').on('click', function(e) {
-    e.preventDefault();
-    $('html, body').animate({ 
-    	scrollTop: $($(this).attr('href')).offset().top }, 800, 'linear');
+
+/* Smooth scroll version 3*/
+$(function(){
+	$('#js-scroll-about').on("click", function(e){
+			e.preventDefault();
+			var aboutOffset = $('#js-about').offset().top;
+			$("html, body").animate({
+				scrollTop: aboutOffset
+			}, 800);
+	});
+
+		/* Fixed header when scroll
+    ====================================*/
+
+   var headerHeight = $("#js-header").height();
+   	$(document).on("scroll", function(){
+
+   			var documentScroll = $(this).scrollTop();
+   			
+
+   			if (documentScroll > headerHeight){
+   					$("#js-nav-wrap").addClass("nav-fixed");
+   			}
+   			else {
+   				$("#js-nav-wrap").removeClass("nav-fixed");
+   			}
+
+   	});
+
+
+/* Smooth scroll to the pages block
+    ====================================*/
+
+  $(".menu__link").on("click", function(e){
+
+  	e.preventDefault();
+  	var currentBlock = $(this).attr("href"),
+  			currentBlockOffset = $(currentBlock).offset().top;
+
+  	$("html, body").animate({
+            scrollTop: currentBlockOffset - 40
+        }, 800);
   });
+
 });
+
+
+ 
 
