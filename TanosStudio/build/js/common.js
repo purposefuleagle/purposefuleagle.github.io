392fb11060e5e12bf2ab1076157264169c31f872
Rostislav Ses <rostislavses97@gmail.com>
2019-03-29 14:43:42 +0200

    Сверстана и адаптирована секция Our Team

--- TanosStudio/build/js/common.js
+++ TanosStudio/build/js/common.js
@@ -23,5 +23,34 @@ $(function() {
   	fade: true,
   	cssEase: 'linear'
 	});
-
+	$('.our-employees').slick({
+		infinite: true,
+		arrows: false,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		autoplay: true,
+  	autoplaySpeed: 2000,
+		speed: 400,
+		dots: true,
+		responsive: [
+			{
+				breakpoint: 1080,
+				settings: {
+					slidesToShow: 3
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 576,
+				settings: {
+					slidesToShow: 1
+				}
+			}
+		]
+	});
 });
