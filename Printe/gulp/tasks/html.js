f243186e3d7d441ffe1336bd7b5bdd4eaf315a8e
Rostislav <rostislavses97@gmail.com>
2020-02-27 19:38:22 +0200

    Add news section no adaptive

--- Printe/gulp/tasks/html.js
+++ Printe/gulp/tasks/html.js
@@ -1,7 +1,8 @@
-module.exports = function () {
-  $.gulp.task('html', function () {
-    return $.gulp.src('*.html')
-        .pipe($.gulp.dest('build'))
-        .on('end', $.browserSync.reload);
+module.exports = function() {
+  $.gulp.task("html", function() {
+    return $.gulp
+      .src("*.html")
+      .pipe($.gulp.dest("build"))
+      .on("end", $.browserSync.reload);
   });
 };
