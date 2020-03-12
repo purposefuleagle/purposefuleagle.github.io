d774d3b92921627ec52bb074203cd796de1ca2fa
Rostislav <rostislavses97@gmail.com>
2020-03-12 11:09:34 +0200

    Start project Test

---
+++ Test/gulp/config.js
@@ -0,0 +1,14 @@
+module.exports = [
+  './gulp/tasks/sass',
+  './gulp/tasks/html',
+  './gulp/tasks/css',
+  './gulp/tasks/scripts',
+  './gulp/tasks/allimg',
+  './gulp/tasks/images',
+  './gulp/tasks/svg',
+  './gulp/tasks/svg_custom',
+  './gulp/tasks/serve',
+  './gulp/tasks/copy',
+  './gulp/tasks/clean',
+  './gulp/tasks/build',
+];
