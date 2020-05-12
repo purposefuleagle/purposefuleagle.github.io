10c2bb4c5fdf6cfebfef0d6fc8e74bf308e992b1
Rostislav <rostislavses97@gmail.com>
2020-05-12 12:54:09 +0300

    Add small block

---
+++ Denta/build/static/js/main.js
@@ -0,0 +1,65 @@
+"use strict";
+
+$(document).ready(function () {
+  svg4everybody({});
+  /* Banner slider */
+
+  var bannerSlider = function bannerSlider() {
+    $('.js-banner').slick({
+      slideToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      dots: true,
+      customPaging: function customPaging(slider, i) {
+        return '<a href="#" class="banner-slider__dot"></a>';
+      },
+      appendDots: '.banner-slider__dots'
+    });
+  };
+
+  bannerSlider();
+}); // Полифилы
+// forEach IE 11
+
+if ('NodeList' in window && !NodeList.prototype.forEach) {
+  console.info('polyfill for IE11');
+
+  NodeList.prototype.forEach = function (callback, thisArg) {
+    thisArg = thisArg || window;
+
+    for (var i = 0; i < this.length; i++) {
+      callback.call(thisArg, this[i], i, this);
+    }
+  };
+} // closest IE 11
+
+
+(function () {
+  if (!Element.prototype.closest) {
+    Element.prototype.closest = function (css) {
+      var node = this;
+
+      while (node) {
+        if (node.matches(css)) return node;else node = node.parentElement;
+      }
+
+      return null;
+    };
+  }
+})(); // matches IE 11
+
+
+(function () {
+  if (!Element.prototype.matches) {
+    Element.prototype.matches = Element.prototype.matchesSelector || Element.prototype.webkitMatchesSelector || Element.prototype.mozMatchesSelector || Element.prototype.msMatchesSelector;
+  }
+})(); //Array.form IE 11
+
+
+if (!Array.from) {
+  Array.from = function (object) {
+    'use strict';
+
+    return [].slice.call(object);
+  };
+}
